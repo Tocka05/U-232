751f0afe09f1435ef90af95f52bf18730f57486f
Tocka05 <amurvasiliev@gmail.com>
2023-09-23 21:20:10 +0300

    Study
    Васильев Дмитрий Основы программирования У-232

---
+++ 2 задание/2.py
@@ -0,0 +1,33 @@
+#1
+'''import math
+x=(14.26)
+y=(-1.22)
+z=(0.035)
+a=2*math.cos(x-2/3)
+b=1/2+math.sin(y)**2
+c=(1+(z**2)/(3-(z**2)/5))
+d=a/b
+s=d*c
+print(s)'''
+#7
+'''import math
+x=(0.1722)
+y=(6.33)
+z=(0.000325)
+a=5*math.atan(x)
+b=1/4*math.acos(x)
+c=x+3*(abs(x-y))+x**2
+d=(abs(x-y))*z+x**2
+q=c/d
+e=q*b
+s=a-e
+print(s)'''
+#4
+import math
+x=(4000)
+y=(-0.875)
+z=(-0.000475)
+a=(abs(math.cos(x)-math.cos(y)))**(1+2*(math.sin(y)**2))
+b=(1+z+(z**2)/(2+z**3)/3+(z**4)/4)
+s=a*b
+print(s)
